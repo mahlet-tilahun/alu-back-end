f513a6aec2ff2bb8a0f66a542c1a5c014deffd75
mahlet-tilahun <m.tilahun@alustudent.com>
2025-02-14 11:15:07 -0800

    PEP8 validation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,20 +10,15 @@ import sys
 
 if __name__ == "__main__":
     session = requests.Session()
-    
     emp_id = sys.argv[1]
     tasks_url = f'https://jsonplaceholder.typicode.com/users/{emp_id}/todos'
     user_url = f'https://jsonplaceholder.typicode.com/users/{emp_id}'
-    
     tasks_response = session.get(tasks_url)
     user_response = session.get(user_url)
-    
     tasks = tasks_response.json()
     emp_name = user_response.json().get('name')
-    
     completed_tasks = [task for task in tasks if task['completed']]
-    
-    print(f"Employee {emp_name} is done with tasks({len(completed_tasks)}/{len(tasks)}):")
+    print(f"Employee {emp_name} is done with tasks"
+      f"({len(completed_tasks)}/{len(tasks)}):")
     for task in completed_tasks:
         print(f"\t {task['title']}")
-
