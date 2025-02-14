ab0d582a2e64fb650107d34d72e0fede9f014f5a
mahlet-tilahun <m.tilahun@alustudent.com>
2025-02-14 12:02:15 -0800

    PEP8 validation

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -9,30 +9,23 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    session = requests.Session()
-    
+    session = requests.Session()    
     emp_id = sys.argv[1]
     tasks_url = f'https://jsonplaceholder.typicode.com/users/{emp_id}/todos'
     user_url = f'https://jsonplaceholder.typicode.com/users/{emp_id}'
-    
     tasks_response = session.get(tasks_url)
     user_response = session.get(user_url)
-    
     tasks = tasks_response.json()
     emp_data = user_response.json()
     emp_name = emp_data.get('name')
     username = emp_data.get('username')
-    
     completed_tasks = [task for task in tasks if task['completed']]
-    
     print(f"Employee {emp_name} is done with tasks({len(completed_tasks)}/{len(tasks)}):")
     for task in completed_tasks:
-        print(f"\t {task['title']}")
-    
+        print(f"\t {task['title']}") 
     file_name = f"{emp_id}.csv"
-    
     with open(file_name, "w", newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
         for task in tasks:
-            writer.writerow([emp_id, username, task['completed'], task['title']])
-
+            writer.writerow(
+                    [emp_id, username, task['completed'], task['title'])
